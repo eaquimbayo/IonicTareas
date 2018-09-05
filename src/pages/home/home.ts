9ff0bff83c9ebdf7be6505a39a9e346f9df42af6
eaquimbayo <eaquimbayo@hotmail.com>
2018-09-05 11:07:36 -0500

    save201809051107

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { TareaProvider } from '../../providers/tarea/tarea';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  tareas = [];
+  constructor(
+    public navCtrl: NavController,
+    private alerta: AlertController,
+    private servicioTarea: TareaProvider
+  ) {
+this.tareas=servicioTarea.obtenerTarea();
   }
-
+  agregarTarea() {
+    let alert = this.alerta.create({
+      title: "agragar tarea",
+      inputs: [{
+        name: "textoTarea",
+        type: "text"
+      }],
+      buttons: [
+        {
+          text: "Cancelar"
+        },
+        {
+          text: "Agregar",
+          handler: (datos) => {
+            console.log(datos);
+            //this.tareas.push(datos.textoTarea);
+            this.servicioTarea.agregarTarea(datos.textoTarea);
+          }
+        }
+      ]
+    })
+    alert.present();
+    //this.tareas.push("nueva tarea");
+    //console.log(this.tareas);
+  }
 }
---
+++ src/providers/tarea/tarea.ts
@@ -0,0 +1,32 @@
+//import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the TareaProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class TareaProvider {
+  tareas=[];
+
+  constructor() {
+    console.log('Hello TareaProvider Provider');
+  }
+  obtenerTarea(){
+    return this.tareas;
+  }
+  agregarTarea(tarea)
+  {
+this.tareas.push(tarea);
+  }
+archivarTarea()
+  {
+    
+  }
+  editarTarea()
+  {
+    
+  }
+}
